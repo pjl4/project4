d39ce308a74fe57786ee71e21aec69e80b40ce1b
pjl4 <pj.lutz17@gmail.com>
2020-03-13 10:01:32 -0400

    Updates

---
+++ precache-manifest.9c285e459308b1936fa7c69bdf584592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a718b52cc4e2f96c0981c54b0f75b179",
+    "url": "/project4/index.html"
+  },
+  {
+    "revision": "20165707d1725df5ca9e",
+    "url": "/project4/static/css/main.8469320b.chunk.css"
+  },
+  {
+    "revision": "6bcca52db16ec86ab74f",
+    "url": "/project4/static/js/2.38a8bfdb.chunk.js"
+  },
+  {
+    "revision": "b9f28c176efdd716457c02939f4eac67",
+    "url": "/project4/static/js/2.38a8bfdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20165707d1725df5ca9e",
+    "url": "/project4/static/js/main.ff5cd439.chunk.js"
+  },
+  {
+    "revision": "1111ba589acee8523da6",
+    "url": "/project4/static/js/runtime-main.2f183956.js"
+  }
+]);

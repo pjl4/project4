3cf61ccdca8a5ae58d8a113565f3bb4b66b12b31
pjl4 <pj.lutz17@gmail.com>
2020-03-13 09:16:34 -0400

    added backend functionality to download and save CSV to DB if its 3 hours old

--- backend/controllers/downloadNCAAB.js
+++ backend/controllers/downloadNCAAB.js
@@ -1,9 +1,11 @@
 var download = require('download-file');
 const csv = require('csv-parser');
 const fs = require('fs');
+const moment = require('moment');
 var url = 'http://www.thepredictiontracker.com/ncaabbpreds.csv';
+const today = moment().startOf('day');
 let data = {
-	date: Date.now(),
+	date: new Date(),
 	data: []
 };
 const NCAAB = require('../db/models/NCAAB');
@@ -16,18 +18,42 @@ download(url, options, function(err) {
 	if (err) throw err;
 	console.log('Downloaded File');
 });
-
+let updateInfoInDB = false;
 fs.createReadStream('../downloads/NCAAB/ncaabbpreds.csv')
 	.pipe(csv())
 	.on('data', (row) => {
 		data.data.push(row);
-		console.log(row);
 	})
 	.on('end', () => {
 		console.log('CSV file successfully processed');
 		console.log(
 			'*********************************************************'
 		);
-		NCAAB.create(data);
-		console.log(data);
+
+		NCAAB.find({
+			date: {
+				$gte: today.toDate(),
+				$lte: moment(today)
+					.endOf('day')
+					.toDate()
+			}
+		})
+			.then((objects) => {
+				objects.map((item) => {
+					if (
+						parseInt(moment(item.date).format('HH')) + 3 >
+						parseInt(moment(today).format('HH'))
+					) {
+						updateInfoInDB = false;
+					} else {
+						updateInfoInDB = true;
+					}
+				});
+			})
+			.catch(console.error);
 	});
+if (updateInfoInDB) {
+	NCAAB.create(data)
+		.then((data) => console.log('Added data to DB'))
+		.catch(console.error);
+}

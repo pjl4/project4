e07b3fc35f9500235db551d78de1a1c01f2a860e
pjl4 <pj.lutz17@gmail.com>
2020-03-13 10:18:24 -0400

    changed some variables for deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -53,10 +53,16 @@ class App extends React.Component {
 	};
 
 	render() {
+		let loadSports = localStorage.id && this.state.home;
+		let noOddsData =
+			this.state.formattedGames.length === 0 &&
+			this.state.home &&
+			!this.state.loading &&
+			localStorage.id;
 		return (
 			<div className="App">
 				<Header></Header>
-				{localStorage.id && this.state.home && (
+				{loadSports && (
 					<SportsList
 						setClickedSport={this.setClickedSport}
 					></SportsList>
@@ -111,17 +117,12 @@ class App extends React.Component {
 						loading={this.state.loading}
 					/>
 				)}
-				{this.state.formattedGames.length === 0 &&
-					this.state.home &&
-					!this.state.loading &&
-					localStorage.id && <h1>No Odds data to display</h1>}
+				{noOddsData && <h1>No Odds data to display</h1>}
 				{this.state.formattedGames.length > 0 &&
 					this.state.formattedGames.map((game, index) => (
 						<Odds key={index} game={game}></Odds>
 					))}
-				{!localStorage.id && this.state.home && (
-					<h3>Log In to see in season sports</h3>
-				)}
+				{!loadSports && <h3>Log In to see in season sports</h3>}
 			</div>
 		);
 	}
